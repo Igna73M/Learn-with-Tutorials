a1f10f121c4012d8eba975d400118e9cae09e760
Mugabe Ignace <ignacemugabe@gmail.com>
2023-08-04 07:53:34 +0200

    Add Facebook clone website with vanila javascript

---
+++ SOCIALBOOK/script.js
@@ -0,0 +1,35 @@
+const settingsMenu = document.querySelector(".settings-menu");
+const darkBtn = document.getElementById("dark-btn");
+
+function settingsMenuToggle ()
+{
+    settingsMenu.classList.toggle("settings-menu-height");
+}
+
+darkBtn.onclick = function ()
+{
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if (localStorage.getItem("theme") == "light")
+    {
+        localStorage.setItem("theme", "dark");
+    } else
+    {
+        localStorage.setItem("theme", "light");
+    }
+
+}
+
+if (localStorage.getItem("theme") == "light")
+{
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark")
+{
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+} else
+{
+    localStorage.setItem("theme", "light");
+}
